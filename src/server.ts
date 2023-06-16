e259a9619f0ea0aa3b481ec6e47522f61a2bb19d
hpmartins <hpmartins@gmail.com>
2023-06-15 19:28:32 -0700

    disabling cors?

--- src/server.ts
+++ src/server.ts
@@ -76,14 +76,14 @@ export class Wolfgang {
 
     app.use('/static', express.static(path.join(__dirname, 'public')))
 
-    app.use(cors(), function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
-      next();
-    });
+    // app.use(cors(), function(req, res, next) {
+    //   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    //   res.header(
+    //     "Access-Control-Allow-Headers",
+    //     "Origin, X-Requested-With, Content-Type, Accept"
+    //   );
+    //   next();
+    // });
 
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
